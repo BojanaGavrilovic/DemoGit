4860a6277b090b164bab249da3f0c2bb098fc410
BojanaGavrilovic <bojanatojaga@gmail.com>
2021-02-04 21:40:26 +0100

    dodat navigate to

--- src/tests/DemoTests.java
+++ src/tests/DemoTests.java
@@ -16,6 +16,7 @@ System.setProperty("webdriver.chrome.driver", "driver-lib\\chromedriver.exe");
 		
 		
 		WebDriver driver = new ChromeDriver();
+		driver.navigate().to("https://www.google.com");
 		
 	}
 	
